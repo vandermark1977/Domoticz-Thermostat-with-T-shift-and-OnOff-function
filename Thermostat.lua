bb835d98ade2e675d7bffa045d4e99392699f7ae
vandermark1977 <49677940+vandermark1977@users.noreply.github.com>
2021-02-03 19:34:30 +0100

    Update Thermostat.lua

--- Thermostat.lua
+++ Thermostat.lua
@@ -17,7 +17,7 @@ return {
         local wpSwitchId = 60                       -- Heatpump_State
         local DefrostSwitchId = 81                  -- Defrost_State
         local target_temp = domoticz.devices(66)    -- Fill in IDX of the Pana [Main_Target_Temp]
-        local ShiftManual = domoticz.devices(149)   -- Fill in IDX of Your Manual TaShift [temperature thermostat]
+        local ShiftManual = domoticz.devices(149)   -- Fill in IDX of Your Manual TaShift [Device: thermostat setpoint]
         local Shift = 0                             -- Local variable
         local roomTemperature = tonumber(domoticz.devices(roomTemperatureId).rawData[1])
         local setPoint = domoticz.utils.round(domoticz.devices(thermostaat).setPoint, 2)
